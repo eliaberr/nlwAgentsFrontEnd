f5ea0d0c30d646cd7731b7922ea3f6e313d54a73
Eliabr Rodrigues <eliabe859@gmail.com>
2025-07-10 16:13:36 -0300

    first commit

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -4,7 +4,7 @@ import type { RoomParams } from "types";
 export function Room() {
   const params = useParams<RoomParams>();
   {/*if (!params.roomID) {
-    return <Navigate replace to="/" />;
+    return <Navigate replace to="/" /> ;
   }*/}
 
   return <div>Room Datails {JSON.stringify(params)}</div>;
